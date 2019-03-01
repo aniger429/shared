fd2a26cce1d588f2b5919e68eb6e5c3180767d70
aniger429 <regina.c.balajadia@gmail.com>
2019-03-02 05:58:08 +0800

    Updated funky

--- funky.js
+++ funky.js
@@ -1,13 +1,7 @@
+alert("Hello from CDN!!!");
 function myFunc() {
-
-require(['N/record'],function (r)
-    {
-    var p = r.load.promise({type: r.Type.CUSTOMER, id: 1633});
-    p.then(function (cust)
-    {
-    console.log(JSON.stringify(cust));
-     });
-    }
-
-
-}
+               require(['N/record'], function (r) {
+                   var p = r.load.promise({type: r.Type.CUSTOMER, id: 13787});
+                   p.then(function (cust) { console.log(JSON.stringify(cust)); } );
+               });
+           }
